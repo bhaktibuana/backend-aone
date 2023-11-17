7dd8949b0ef95a13e88c6c453bd44bc793b6cfde
Bhakti Mega Buana <bhaktibuana19@gmail.com>
2023-11-17 22:49:30 +0700

    feat(logger): Fix Bug - App Logger

--- src/middlewares/device/device.middleware.ts
+++ src/middlewares/device/device.middleware.ts
@@ -6,12 +6,11 @@ import { response } from "@/utils";
 class Device {
   constructor() {}
 
-  private detector: DeviceDetector = new DeviceDetector();
-
   public detect(req: Request, res: Response, next: NextFunction): void {
     const userAgent: string = req.headers["user-agent"] as string;
+    const detector: DeviceDetector = new DeviceDetector();
     const device: DeviceDetector.DeviceDetectorResult =
-      this.detector.parse(userAgent);
+      detector.parse(userAgent);
 
     if (device.bot !== null) {
       response<{ device: DeviceDetector.DeviceDetectorResult }>(
