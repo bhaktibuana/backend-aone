06905c3e189f0d0a51494ae9d58104c4d39bce20
Bhakti Mega Buana <bhaktibuana19@gmail.com>
2023-11-19 00:54:33 +0700

    feat(auth): Fix Bug Webpack Piblic Assets Path

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: "production",
@@ -25,5 +26,10 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [{ from: "./src/assets", to: "assets" }],
+    }),
+  ],
   externals: ["pg-hstore"],
 };
