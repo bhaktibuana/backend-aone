619fc62b0dbe6e3306af68d5988fa6dd8ebb191f
bhaktibuana <bhaktibuana19@gmail.com>
2023-12-09 11:45:11 +0700

    feat(auth): Refactor Unit Test - generateOtp & verifyOtp

--- __tests__/unit/utils/otp.util.spec.ts
+++ __tests__/unit/utils/otp.util.spec.ts
@@ -1,56 +1,127 @@
-import { generateOtp, verifyOtp, generateJwt } from "@/utils";
+import { generateOtp, verifyOtp } from "@/utils";
 
-describe("OTP", () => {
-  it("Should be able to generate OTP (4 characters)", () => {
-    const { otp } = generateOtp();
-    expect((otp as string).length).toBe(4);
+import { TGenerateOtp, IVerifyOtp, TOtpToken } from "@/types";
+
+jest.mock("otp-generator", () => ({
+  generate: jest.fn(),
+}));
+
+jest.mock("@/utils/generateJwt.util", () => ({
+  generateJwt: jest.fn(),
+}));
+
+jest.mock("@/utils/verifyJwt.util", () => ({
+  verifyJwt: jest.fn(),
+}));
+
+describe("generateOtp", () => {
+  beforeEach(() => {
+    const validPayload: TOtpToken = {
+      error: null,
+      decoded: { otp: "1234" },
+    };
+
+    (require("otp-generator").generate as jest.Mock).mockReturnValue("1234");
+
+    (
+      require("@/utils/generateJwt.util").generateJwt as jest.Mock
+    ).mockReturnValue("mockedJwtToken");
+
+    (require("@/utils/verifyJwt.util").verifyJwt as jest.Mock).mockReturnValue(
+      validPayload
+    );
   });
 
-  it("Should be able to generate string", () => {
-    const { otp } = generateOtp();
-    expect(typeof otp).toBe("string");
+  it("should generate OTP and return decoded information", () => {
+    const result: TGenerateOtp = generateOtp();
+    expect(result).toEqual({
+      otp: "1234",
+      otpToken: "mockedJwtToken",
+    });
   });
 
-  it("Should be able to verify OTP", () => {
-    const { otp, otpToken } = generateOtp();
-    const response = verifyOtp(otp as string, otpToken);
-    expect(response).toEqual({
+  it("Should be able to generate String", () => {
+    const result: TGenerateOtp = generateOtp();
+    expect(typeof result.otp).toBe("string");
+  });
+
+  it("Should generate OTP (4 characters)", () => {
+    const result: TGenerateOtp = generateOtp();
+    expect((result.otp as string).length).toBe(4);
+  });
+});
+
+jest.mock("@/utils/verifyJwt.util", () => ({
+  verifyJwt: jest.fn(),
+}));
+
+describe("verifyOtp", () => {
+  beforeAll;
+  it("should return 'OTP verified' for a valid OTP", () => {
+    const validPayload: TOtpToken = {
+      error: null,
+      decoded: { otp: "1234" },
+    };
+    (require("@/utils/verifyJwt.util").verifyJwt as jest.Mock).mockReturnValue(
+      validPayload
+    );
+
+    const result: IVerifyOtp = verifyOtp("1234", "validToken");
+
+    expect(result).toEqual({
       verified: true,
       message: "OTP verified",
     });
   });
 
-  it("Should error if has wrong OTP", () => {
-    const { otpToken } = generateOtp();
-    const response = verifyOtp("@2923", otpToken);
-    expect(response).toEqual({
-      verified: false,
-      message: "Wrong OTP",
-    });
-  });
+  it("should return 'Invalid token' for an invalid token", () => {
+    const invalidPayload: TOtpToken = {
+      error: { name: "" } as TOtpToken["error"],
+      decoded: {},
+    };
+    (require("@/utils/verifyJwt.util").verifyJwt as jest.Mock).mockReturnValue(
+      invalidPayload
+    );
 
-  it("Should error if has invalid token", () => {
-    const { otp } = generateOtp();
-    const response = verifyOtp(otp as string, "oqiuwhfbi2u3b123");
-    expect(response).toEqual({
+    const result: IVerifyOtp = verifyOtp("1234", "invalidToken");
+
+    expect(result).toEqual({
       verified: false,
       message: "Invalid token",
     });
   });
 
-  it("Should error if token expired", () => {
-    const { otp } = generateOtp();
-    const otpToken = generateJwt(
-      {
-        otp,
-      },
-      "-1s"
+  it("should return 'OTP expired' for an expired token", () => {
+    const expiredPayload: TOtpToken = {
+      error: { name: "TokenExpiredError" } as TOtpToken["error"],
+      decoded: {},
+    };
+    (require("@/utils/verifyJwt.util").verifyJwt as jest.Mock).mockReturnValue(
+      expiredPayload
     );
 
-    const response = verifyOtp(otp as string, otpToken);
-    expect(response).toEqual({
+    const result: IVerifyOtp = verifyOtp("1234", "expiredToken");
+
+    expect(result).toEqual({
       verified: false,
       message: "OTP expired",
+    });
+  });
+
+  it("should return 'Wrong OTP' for a wrong OTP", () => {
+    const validPayload: TOtpToken = {
+      error: null,
+      decoded: { otp: "4321" },
+    };
+    (require("@/utils/verifyJwt.util").verifyJwt as jest.Mock).mockReturnValue(
+      validPayload
+    );
+
+    const result: IVerifyOtp = verifyOtp("1234", "validToken");
+
+    expect(result).toEqual({
+      verified: false,
+      message: "Wrong OTP",
     });
   });
 });
