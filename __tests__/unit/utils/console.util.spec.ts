7565b1000da3e8c1a902e88d7045ed748fd1c600
bhaktibuana <bhaktibuana19@gmail.com>
2023-12-09 13:04:24 +0700

    feat(auth): Refactor Unit Test - consoleLog & consoleError

--- __tests__/unit/utils/console.util.spec.ts
+++ __tests__/unit/utils/console.util.spec.ts
@@ -1,11 +1,86 @@
 import { consoleError, consoleLog } from "@/utils";
 
-describe("Console", () => {
-  it("Should be able to console error", () => {
-    consoleError("Testing Console Error", null);
+jest.mock("@/configs/app.config", () => {
+  const originalConfig = jest.requireActual("@/configs/app.config");
+
+  return {
+    __esModule: true,
+    ...originalConfig,
+    config: {
+      nodeEnv: originalConfig.config.nodeEnv,
+    },
+  };
+});
+
+const mockConfig = jest.requireMock("@/configs/app.config");
+
+describe("consoleLog", () => {
+  let mockConsoleLog: jest.SpyInstance;
+
+  beforeAll(() => {
+    mockConsoleLog = jest.spyOn(global.console, "log").mockImplementation();
   });
 
-  it("Should be able to console log", () => {
-    consoleLog("Testing Console Log", null);
+  afterAll(() => {
+    mockConsoleLog.mockRestore();
+  });
+
+  beforeEach(() => {
+    mockConsoleLog.mockClear();
+  });
+
+  it("should log to the console in development environment", () => {
+    mockConfig.config.nodeEnv = "development";
+
+    const mockTitle = "Test Title";
+    const mockPayload = { key: "value" };
+
+    consoleLog(mockTitle, mockPayload);
+
+    expect(mockConsoleLog).toHaveBeenCalledWith(
+      `[${mockTitle}] =>`,
+      mockPayload
+    );
+  });
+
+  it("should not log to the console in production environment", () => {
+    mockConfig.config.nodeEnv = "production";
+
+    const mockTitle = "Test Title";
+    const mockPayload = { key: "value" };
+
+    consoleLog(mockTitle, mockPayload);
+
+    expect(mockConsoleLog).not.toHaveBeenCalled();
+  });
+});
+
+describe("consoleError", () => {
+  let mockConsoleError: jest.SpyInstance;
+
+  beforeAll(() => {
+    mockConsoleError = jest.spyOn(global.console, "error").mockImplementation();
+  });
+
+  afterAll(() => {
+    mockConsoleError.mockRestore();
+  });
+
+  beforeEach(() => {
+    mockConsoleError.mockClear();
+  });
+
+  it("should error to the console", () => {
+    mockConfig.config.nodeEnv = "development";
+
+    const mockTitle = "Error";
+    const mockPayload = { key: "value" };
+
+    consoleError(mockTitle, mockPayload);
+
+    expect(mockConsoleError).toHaveBeenCalledWith(
+      `[${mockTitle}] =>`,
+      mockPayload
+    );
   });
 });
